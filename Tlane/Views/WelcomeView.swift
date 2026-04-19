37dae975f6628f7fb83bbd84f3802f8abbd328af
Artemio <espinoa.01@outlook.com>
2026-04-18 19:15:52 -0600

    e

---
+++ Tlane/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Tlane
+//
+//  Created by Dev Jr. 17 on 18/04/26.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
